fa03fad89bee5db63c7fd23b4b25145c46473ee5
Victor Aleman Hidalgo <a.22308051281123@cbtis128.edu.mx>
2024-09-06 10:16:51 -0600

    Add files via upload
    for, while

---
+++ ejemplofor.py
@@ -0,0 +1,3 @@
+materialesEscolares = ["Borrador", "Lapiz", "Sacapuntas", "Colores", "Pluma", "Cuaderno", "Pegamento", "Tijeras"]
+for x in fruits:
+  print(x)
---
+++ ejemplowhile.py
@@ -0,0 +1,4 @@
+millas = 20
+while millas <= 400:
+  print("Voy por las ", millas, "millas.")
+  millas += 20
