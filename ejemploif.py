43ce34ee915dcf0b93fa36ef10687bea6717d984
Victor Aleman Hidalgo <a.22308051281123@cbtis128.edu.mx>
2024-09-06 09:57:09 -0600

    Add files via upload

---
+++ ejemploif.py
@@ -0,0 +1,5 @@
+edad = 15 #Asigna un valor
+if edad >= 18: #Compara o evalua la variable con un valor asignado y determina si es verdadero o falso
+    print("Puedes entrar a bailar")
+else:
+    print("Vete a dormir sssss....")
