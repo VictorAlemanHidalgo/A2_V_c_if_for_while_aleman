95b03dbedb26bc233b6a50c6076c96d5eff61fe4
Victor Aleman Hidalgo <a.22308051281123@cbtis128.edu.mx>
2024-09-06 09:32:45 -0600

    Add files via upload
    subi dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,20 @@
+''' Victor Aleman
+Trabajando con cadenas 
+'''
+nombre = "Victor"
+print(nombre[0])
+print(nombre)
+for c in "Victor":
+  print(c) 
+MisNovias=["E", "l", "i"]
+for chicas in MisNovias:
+  print(chicas)
+  print("otro ejemplo")
+b = "Victor Aleman"
+print(b[2:5])
+a = "victor aleman"
+print(a.upper())
+a = "Maria"
+b = "Perez"
+c = a + " " + b
+print(c)
---
+++ misvariables.py
@@ -0,0 +1,16 @@
+import math
+edad = 17 #El 17 se guarda en edad
+nombre = "Victor Aleman"
+
+print ("Tu edad es ",edad)
+print ("Te llamas ",nombre)
+print("-- tipo de variables --")
+x = str(nombre)    # x will be '3'
+y = int(3.141575746363)    # y will be 3
+z = float(7)
+print(x)
+print(y)
+print(z)
+print("-- funciones matematicas --")
+x = math.sqrt(64)
+print(x)
